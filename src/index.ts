774b811d9146968bfb07b17df39f7af63c951f25
n4n5 <its.just.n4n5@gmail.com>
2024-01-01 23:06:34 +0100

    add

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,28 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
 
-const app = new Hono()
-app.get('/', (c) => c.text('Hello Hono!'))
+const app = new Hono();
+app.get("/", (c) => c.text("Hello Hono!"));
 
-const port = 3000
-console.log(`Server is running on port ${port}`)
+const port = 3000;
+console.log(`Server is running on port ${port}`);
+
+let isOn = false;
+
+const api = new Hono();
+api.get("/isOn", (c) => c.json({ on: isOn }));
+api.post("/setOn", (c) => {
+    isOn = true;
+    return c.json({ isOn: isOn });
+});
+api.post("/setOff", (c) => {
+    isOn = false;
+    return c.json({ isOn: isOn });
+});
+
+app.route("/api", api);
 
 serve({
-  fetch: app.fetch,
-  port
-})
+    fetch: app.fetch,
+    port,
+});
