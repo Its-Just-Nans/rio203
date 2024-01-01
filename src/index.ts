740bad91ab6d31d51d32e6bfcb9c5a4abbc93bc6
n4n5 <its.just.n4n5@gmail.com>
2024-01-01 23:22:46 +0100

    add

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import { serve } from "@hono/node-server";
+import { serveStatic } from "@hono/node-server/serve-static";
 import { Hono } from "hono";
 
 const app = new Hono();
-app.get("/", (c) => c.text("Hello Hono!"));
+app.use("/", serveStatic({ root: "./dist" }));
 
 const port = 3000;
 console.log(`Server is running on port ${port}`);
@@ -10,7 +11,8 @@ console.log(`Server is running on port ${port}`);
 let isOn = false;
 
 const api = new Hono();
-api.get("/isOn", (c) => c.json({ on: isOn }));
+api.get("/", (c) => c.json({ msg: "Hello API" }));
+api.get("/isOn", (c) => c.json({ isOn: isOn }));
 api.post("/setOn", (c) => {
     isOn = true;
     return c.json({ isOn: isOn });
