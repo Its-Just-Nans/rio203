053147a69424b19d8740751daedf0b2a29eb0af6
n4n5 <its.just.n4n5@gmail.com>
2024-01-31 10:31:04 +0100

    add

--- server/websocket/websocketsClients.ts
+++ server/websocket/websocketsClients.ts
@@ -5,11 +5,21 @@ type WsClients = { [e: string]: WebSocket };
 let ws_clients: WsClients = {};
 
 export const setWsClient = (id: string, ws: WebSocket) => {
+    console.log("setWsClient", id);
+    console.log("setWsClient", id);
+    console.log("setWsClient", id);
+    console.log("setWsClient", id);
+    console.log("setWsClient", id);
+    console.log("setWsClient", id);
     ws_clients[id] = ws;
 };
 
 export const sendObjectToClient = (id: string, obj: any) => {
+    const str = JSON.stringify(obj);
+    console.log("sendObjectToClient", id, str);
+    console.log("sendObjectToClient", id, str);
+    console.log("sendObjectToClient", id, str);
     if (ws_clients[id]) {
-        ws_clients[id].send(JSON.stringify(obj));
+        ws_clients[id].send(str);
     }
 };
